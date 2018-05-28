7a4f731ee628cc942cb71cb355082df4ea3f043b
Yggdrasil1 <k.a.i.winkler@gmx.de>
2018-05-28 17:45:55 +0200

    homework done

---
+++ homework_3.py
@@ -0,0 +1,23 @@
+
+def average(liste):
+	sum=0
+	assert len(liste) > 0, "List is emtpy"
+
+	for item in liste:
+		assert type(item) is (int or float), "something is NaN"
+		
+		sum+=item
+
+	return (sum/len(liste))
+
+def median(liste):
+	sorted_list = sorted(liste)
+	if len(liste)%2 == 1:
+		return sorted_list[int(len(sorted_list)/2)]
+	else:
+		return ((sorted_list[int(len(sorted_list)/2)]+sorted_list[int(len(sorted_list)/2-1)])/2.0)
+
+if __name__ == '__main__':
+	
+	print(average([1,2,3,4,5,])) 
+	print(median([1,3,5,7,9,10]))
