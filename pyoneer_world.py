482439205cd2d4c89f751b8462e49209af976c21
Yggdrasil1 <k.a.i.winkler@gmx.de>
2018-07-02 15:49:15 +0200

    first move attempt

--- pyoneer_world.py
+++ pyoneer_world.py
@@ -14,4 +14,27 @@ class World:
         if char == default:
             
             self.character = pyoneer_character.Character("human")
+        
+    def move(self,mv_input):
+        """
+        Method to move the character through the world
+        """
+        
+        allowed_userinput = False     
+        
+        userinput = ""
+        
+        while not allowed_userinput:
+            userinput = str(raw_input('In which direction do you wish to walk? '
+                                    +'[w,a,s,d]: '))
+        
+            if not userinput in ['w','s','a','d']:
+                print("Please only type one of the possible keys.")
+                allowed_userinput = False
+            else:
+                allowed_userinput = True
+                
+        if self.character.x_position == 0 and self.character.y_position == 0:
             
+            if userinput == "w":
+                new_x = 5 + (self.character.level - 1) * 3
