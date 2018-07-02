22cec535b58c28287fce852b87c80cf97de892b2
Yggdrasil1 <k.a.i.winkler@gmx.de>
2018-07-02 19:15:15 +0200

    added more content (first userinout and runable version)

--- capytulate_world.py
+++ capytulate_world.py
@@ -3,15 +3,15 @@
 Created on Mon Jul  2 13:44:05 2018
 """
 
-import pyoneer_character
+import capytulate_character
 import numpy as np
 
 class World:
     
-    def __init__(self,char=default):
+    def __init__(self,char="default"):
         
-        if char == default:
-            self.character = pyoneer_character.Character("human")
+        if char == "default":
+            self.character = capytulate_character.Character("human")
         
         else:
             self.character = char
@@ -38,6 +38,9 @@ class World:
                 allowed_userinput = False
             else:
                 allowed_userinput = True
+                
+        new_x = 0
+        new_y = 0
                 
         if self.character.x_position == 0 and self.character.y_position == 0:
             
@@ -81,13 +84,59 @@ class World:
              
             self.character.x_position += new_x
             self.character.y_position += new_y
+            
+      
+            
+    def analyze_userinput(self,some_input):
+        """
+        checks the userinput and let the character behave accordingly
+        """
+        
+        if some_input in ['move','mv','walk']:
+            self.move()
+            
+        else:
+            print("Sorry, i don't understand.")
              
+             
+    def encounter(self):
+        """
+        checks the position of the character and faces him with a random 
+        encounter if he is not on his safespot (0,0)
+        """
+        
+        x_pos = self.character.x_position
+        y_pos = self.character.y_position
+        
+        if x_pos == 0 and y_pos == 0:
+            print("Ah, back in Town, you are in your safespot!")
+        elif np.absolute(x_pos) == self.worldsize and np.absolute(y_pos) == self.worlsize:
+            print("Bossfight!!")
+        
+                
+    def run(self):
+        """
+        executing all the methods to let the game flow
+        """  
+        
+        exiting = False
+        
+        while not exiting:
+            userinput_game = str(raw_input("What would you like to do?: "))
+            
+            if userinput_game in ['Exit','exit','logout','Logout','log out','Log out']:
+                exiting = True
+                print("See you soon!")
+            
+            else:
+                self.analyze_userinput(userinput_game)
                 
                 
-                
-                
-                
-                
+if __name__ == "__main__":
+        
+        new_game = World()
+        new_game.run()
+        
                 
                 
                 
